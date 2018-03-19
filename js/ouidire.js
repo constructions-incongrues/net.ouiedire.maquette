6008b8054c914ea5b2bb99b08e0a0080096218df
kesey <meunier.fabien@yahoo.fr>
2018-03-19 18:24:50 +0100

    add wavesurfer player

--- js/ouidire.js
+++ js/ouidire.js
@@ -1,5 +1,47 @@
 $(document).ready(function() {
 
+	/*************************************************create player instance****************************************************/
+
+	var wavesurfer;
+
+	wavesurfer = WaveSurfer.create({
+	    container: '#waveform',
+	    waveColor: '#ffffff',
+	    progressColor: '#808080',
+	    barHeight: 1,
+	    barWidth: 1,
+	    height: 100,
+	    hideScrollbar: true,
+	    backend: 'MediaElement'
+	});
+
+	/*************************************************get audio source*********************************************************/
+
+	var audio_source;
+
+	audio_source = $("#waveform").attr("data-src");
+
+	wavesurfer.load(audio_source);
+
+	/*************************************************start / pause player*****************************************************/
+
+	$(".transport-play").click(function() {
+		if (wavesurfer.isPlaying()) {
+			wavesurfer.pause();
+			$(this)
+				.find(".white")
+				.toggleClass('fa-play fa-pause');
+		} else {
+			wavesurfer.play();
+			$(this)
+				.find(".white")
+				.toggleClass('fa-pause fa-play');
+		}
+		//wavesurfer.playPause();
+	});
+
+	/************************************************flyer visual expand********************************************************/
+
 	var set_flyer_src;
 
     set_flyer_src = function (flyer_src) {
